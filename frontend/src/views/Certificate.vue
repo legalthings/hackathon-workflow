95af31e6890300abc98ab0f162b9628c52894322
fexra <fexra@protonmail.com>
2019-04-13 20:48:41 +0200

    certicate router

--- frontend/src/views/Certificate.vue
+++ frontend/src/views/Certificate.vue
@@ -112,7 +112,7 @@ import Vue from 'vue'
 import { Component, Watch, Prop } from 'vue-property-decorator'
 
 @Component
-export default class License extends Vue {
+export default class Certificate extends Vue {
   @Prop(String) readonly chainId!: string
 }
 </script>
