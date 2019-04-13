3136e3245da57f6805f11d191dd3772a7953e225
fexra <fexra@protonmail.com>
2019-04-13 16:00:02 +0200

    license template

---
+++ frontend/src/views/License.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-card class="main mx-auto elevation-15" max-width="60rem" height="100%">
+    <v-layout row wrap justify-content-space-between>
+    
+  </v-card>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Component, Watch } from 'vue-property-decorator'
+// @ts-ignore
+import { QrcodeStream } from 'vue-qrcode-reader'
+
+@Component({
+  components: {
+    QrcodeStream
+  }
+})
+export default class Home extends Vue {}
+</script>
+
+<style scoped>
+.main {
+  background-image: linear-gradient(-90deg, #7daef4 0%, #4776e6 100%);
+}
+</style>
