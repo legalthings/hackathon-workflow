6a8d91f126a7d65255cf703f7f58127418b0041a
Michael Krens <mkrens@gmail.com>
2019-04-13 16:17:13 +0200

    add backend proxy

---
+++ frontend/vue.config.js
@@ -0,0 +1,15 @@
+const proxyUrls = ['^/api']
+const proxy = {}
+proxyUrls.forEach(route => {
+  proxy[route] = {
+    target: 'http://localhost:8080'
+  }
+})
+
+module.exports = {
+  devServer: {
+    proxy,
+    port: 9000,
+    disableHostCheck: true
+  }
+}
